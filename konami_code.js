9b112de29ddba8501cc80033bbc4d978cea47c0d
George  Pianka <gpianka1@gmail.com>
2019-04-27 15:36:31 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -2,7 +2,7 @@ function init() {
   const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
   let index = 0;
   document.body.addEventListener('keydown', function(e){
-    const key = parseInt(e.which || e.keyCode);
+    const key = parseInt(e.which || e.keyCode || e.detail);
     console.log(e.which);  
     console.log(e.keyCode);
     if (key === code[index]) {
